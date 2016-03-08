7782c2e8ce2c4725facf9470ebe88b7a08e72174
achristoph <achristoph@gmail.com>
2016-03-08 14:57:32 -0600

    config files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: __dirname + "/app/main.ts",
+  output: {
+    path: __dirname + "/app",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.ts$/, loader: "tslint"
+      }
+    ],
+    loaders: [
+      { test: /\.ts?$/, loader: 'ts-loader' }
+    ]
+  },
+  tslint: {
+  }
+};
