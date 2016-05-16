37f9e57a16644130ad2e20325ac4e2cb9e5bb677
Arthur Christoph <achristoph@gmail.com>
2016-05-16 14:07:46 -0500

    update readme, packages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Typescript Starter
+This is a basic JS starter project using Typescript, Babel, ES6, ESLint, TSLint and Webpack
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,3 @@
-import * as _ from 'lodash';
 import * as greet from './greet';
 
-let output = _.capitalize(greet.hello());
-console.log(output);
+console.log(greet.hello());
